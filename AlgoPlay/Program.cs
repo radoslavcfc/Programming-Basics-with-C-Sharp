ab3737df4539c6503aa9eaa1e7d02861eddf5c91
Radoslav Hadzhiiliev <radoslavcfc05@gmail.com>
2020-06-22 17:38:17 +0300

    update

--- AlgoPlay/Program.cs
+++ AlgoPlay/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 
-
 namespace AlgoPlay
 {
     public class Program
@@ -10,12 +9,27 @@ namespace AlgoPlay
         static void Main(string[] args)
         {
             var result = FindTwoSum(new List<int>() { 3, 1, 5, 7, 5, 9 }, 10);
-            Console.WriteLine(result);
+            if (result != null)
+            {
+                Console.WriteLine(result.Item1 + ", " + result.Item2);
+            }
         }
 
         private static Tuple<int,int> FindTwoSum(List<int> list, int sum)
         {
-
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                for (int j = i + 1; j < list.Count - i - 1; j++)
+                {
+                    var current = list[i];
+                    var adjacent = list[j];
+                    if (current + adjacent == sum)
+                    {
+                        return new Tuple<int, int>(i, (j));
+                    }
+                }
+            }
+            
             return null;
         }
     }
