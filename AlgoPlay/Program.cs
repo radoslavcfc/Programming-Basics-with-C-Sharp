28720f0bb1fd654c0e3bf1a6373664e8514382b5
Radoslav Hadzhiiliev <radoslavcfc05@gmail.com>
2020-06-22 17:04:10 +0300

    sceleton

--- AlgoPlay/Program.cs
+++ AlgoPlay/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace AlgoPlay
 {
-    class Program
+    public class Program
     {
         
         static void Main(string[] args)
         {
-           
+            var result = FindTwoSum(new List<int>() { 3, 1, 5, 7, 5, 9 }, 10);
+            Console.WriteLine(result);
         }
 
+        private static Tuple<int,int> FindTwoSum(List<int> list, int sum)
+        {
+
+            return null;
+        }
     }
    
 }
